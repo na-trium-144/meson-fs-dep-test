55c076d67d70d90f2c92c4968320bc63b27525df
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2025-08-27 20:16:46 +0900

    check experimental filesystem

--- main.cc
+++ main.cc
@@ -1,6 +1,13 @@
-#include <filesystem>
 #include <iostream>
+#ifdef EXPERIMENTAL_FS
+#include <experimental/filesystem>
+namespace std_fs = std::experimental::filesystem;
+#else
+#include <filesystem>
+namespace std_fs = std::filesystem;
+#endif
+
 
 int main(){
-    std::cout << std::filesystem::exists("foo") << std::endl;
+    std::cout << std_fs::exists("foo") << std::endl;
 }
